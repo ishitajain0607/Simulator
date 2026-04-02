3db1f36b181c8693dce24d47c554a2f0ee83ecfb
Ishita <ishitajain0607@gmail.com>
2026-04-02 13:39:04 +0530

    Edited Architecture

--- Main.py
+++ Main.py
@@ -4,20 +4,20 @@ def binary_to_signed_decimal(binary_string):
 def binary_to_unsigned_decimal(num):
     pass
 
-def decimal_to_binary(num):
+def decimal_to_binary(num, size):
     #must handle both signed and unsigned representation
     #unsignedrange-> 0 to 2^32-1
     #signedrange-> - -2^31 to 2^31-1
-    #copied from assembler code modified a little
+    #copied from assembler code and modified a little
     s=''
-    num= num%(2**32)
+    num= num%(2**size)
     while (num!=0):
         if (num%2==0):
             s='0'+s
         else:
             s='1'+s
         num=num//2
-    s=(32-len(s))*'0'+s
+    s=(size-len(s))*'0'+s
     return s
 
 def unsigned_decimal_to_hex(num):
@@ -38,6 +38,12 @@ dataMemory= {}
 instructionMemory= []
 controlSignals= {}
 
+registerFile= dict.fromkeys([decimal_to_binary(x, 5) for x in range(32)], '0'*32)
+registerFile['00010']= '00000000000000000000000101111100' #set sp to 380
+
+dataMemory= dict.fromkeys([unsigned_decimal_to_hex(x) for x in range(0x00010000, 0x00010080, 4)], '0'*32)
+
+
 #Decode Phase
 def generateControlSignals(instruction):
     #32 bit instructionString
@@ -47,35 +53,25 @@ def generateControlSignals(instruction):
 def readRegisterFile(instruction):
     #32 bit instructionString
     #extracts rd
-
+    #extracts rs1
+    #extracts rs2
     pass
 
-def extend(instruction, immControl):
+def extend(instruction):
+    #extracts immediate based on control signal
     pass
 
 #Execute phase
 def aluExecute(source1, source2, imm):
     pass
 
-def pcPlusTargetExecute(PC, imm):
-    #you are given jal immediate or branch immediate so you will have to multiply immediate by 2
-    #when computing stuff
-    pass
 
-
-
-#Instruction Fetch and MemoryWrite and RegWrite phases are handled in the run function itself
-'''
-registerFile= {}
-dataMemory= {}
-instructionMemory= []
-'''
 
 def run(input_file):
     f= open(input_file)
     instructionMemory= f.read().split("\n")
     f.close()
-    PC=4
+    PC=4    
     halt= False
     while (halt!=True):
         pass
